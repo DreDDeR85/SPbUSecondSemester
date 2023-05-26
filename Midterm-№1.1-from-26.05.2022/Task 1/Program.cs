828ae0d6e4ccc3818891946cd675fb087295938b
Dmitry Korepin <97905730+DreDDeR85@users.noreply.github.com>
2023-05-26 15:45:51 +0300

    Add files via upload

---
+++ Midterm-№1.1-from-26.05.2022/Task 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    /// <summary>
+    /// Class representing bubble sort for any type of object'
+    /// </summary>
+    /// <typeparam name="T">Values that array for bubble sort contains</typeparam>
+    /// <param name="comparer">Object which sets the comparison relation on our array</param>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The example of sort usage
+
+            List<int> intList = new List<int>() { 7, 4, 2, 10 };
+            Console.WriteLine("Initial data:" + string.Join(",", intList));
+
+            BubbleSort(intList);
+
+            Console.WriteLine("Output data:" + string.Join(",", intList));
+        }
+
+        public static void BubbleSort<T>(IList<T> list, IComparer<T> comparer = null)
+        {
+            if (list == null || list.Count <= 1)
+            {
+                return;
+            }
+            if (comparer == null)
+            {
+                comparer = Comparer<T>.Default;
+                // if the comparer is not specified,
+                // then we will use the standart
+            }
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    if (comparer.Compare(list[j], list[j + 1]) > 0)
+                    {
+                        T temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Midterm-№1.1-from-26.05.2022/Task 1/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NUint.Framework;
+
+namespace BubbleSort.tests;
+
+public class Tests
+{
+    [TestFixture]
+    public class BubbleSortTests
+    {
+        [Test]
+        public void SortInList()
+        {
+            List<int> intList = new List<int>() { 5, 3, 9, -4 };
+            Program.BubbleSort(intList);
+
+            Assert.AreEqual(new List<int>() { -4, 3, 5, 9 }, intList);
+        }
+
+        [Test]
+        public void SortStringArray()
+        {
+            string[] strArr = { "Lord of the Rings", "The Witcher", "The Twelve Chairs", "Harry Potter" };
+            Program.BubbleSort(strArr, Comparer<string>.Default);
+
+            Assert.AreEqual(new string[] { "Harry Potter", "Lord of the Rings", "The Twelve Chairs", "The Witcher" }, strArr);
+        }
+
+        [Test]
+        public void SortCustomObjectList()
+        {
+            List<Statue> statue = new List<Statue>()
+            {
+                new Statue() {Name = "The Motherland Calls", Height = 85, City = "Volgograd"},
+                new Statue() {Name = "Statue of Liberty", Height = 46, City = "New York"},
+                new Statue() {Name = "Christ the Redeemer", Height = 30, City = "Rio de Janeiro"},
+            };
+
+            //sort by the height
+            Program.BubbleSort(statue, Comparer<Statue>.Create((s1, s2) => s1.Height.CompareTo(s2.Height)));
+
+            Assert.AreEqual(new List<Statue>()
+            {
+                new Statue() {Name = "The Motherland Calls", Height = 85, City = "Volgograd"},
+                new Statue() {Name = "Statue of Liberty", Height = 46, City = "New York"},
+                new Statue() {Name = "Christ the Redeemer", Height = 30, City = "Rio de Janeiro"},
+            }, statue;
+        }
+    }
+
+    class Statue
+    {
+        public string Name;
+        public int Height;
+        public string City;
+    }
+}
