1c1e227c9c06b31ce3735db4a7ce5599194aee37
DreDDeR <botayeverydaymyfriend@gmail.com>
2023-10-07 13:56:10 +0300

    better place for files about stack realization

---
+++ Homework2/StackCalculator/StackCalculator/StackCalculator/StackRealization/StackOnArray.cs
@@ -0,0 +1,40 @@
+﻿namespace StackCalculator;
+
+
+/// <summary>
+/// Stask realization by using an array.
+/// </summary>
+public class StackOnArray : IStack
+{
+    public StackOnArray()
+    {
+        stack = new List<int>();
+    }
+
+    private List<int> stack;
+
+    /// <inheritdoc/>
+    public void Push(int value)
+    {
+        stack.Add(value);
+    }
+
+    /// <inheritdoc/>
+    public bool IsEmpty()
+    {
+        return stack.Count == 0;
+    }
+
+    /// <inheritdoc/>
+    public int Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Cannot pop element from empty stack.");
+        }
+
+        var upElement = stack[stack.Count - 1];
+        stack.RemoveAt(stack.Count - 1);
+        return upElement;
+    }
+}
