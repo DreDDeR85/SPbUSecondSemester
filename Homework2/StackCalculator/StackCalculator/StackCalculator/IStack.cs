507ab3a5379a4da7f992be7000ba968a34da744c
DreDDeR <botayeverydaymyfriend@gmail.com>
2023-10-07 13:51:49 +0300

    added three stack's realizations

---
+++ Homework2/StackCalculator/StackCalculator/StackCalculator/IStack.cs
@@ -0,0 +1,28 @@
+﻿namespace StackCalculator;
+
+///<summary>
+/// Stack is a last in - first out container for real values.
+/// </summary>
+public interface IStack
+{
+    ///<summary>
+    /// Add element in stack.
+    /// </summary>
+    /// <param name="value"> Value to add.</param>
+    public void Push(int value);
+
+
+    /// <summary>
+    /// Returns and removes an element from the top of the stack.
+    /// </summary>
+    /// <returns> Top element from the stack.</returns>
+    /// <exception cref="InvalidOperationException"> Not be able to pop an element from empty stack.</exception>
+    public int Pop();
+
+
+    /// <summary>
+    /// Checking for stack empty.
+    /// </summary>
+    /// <returns> True, when the stack is empty, and False, if stack contains at least one element</returns>
+    public bool IsEmpty();
+}
---
+++ Homework2/StackCalculator/StackCalculator/StackCalculator/StackOnList.cs
@@ -0,0 +1,40 @@
+﻿namespace StackCalculator;
+
+/// <summary>
+/// Stack realization by using a linked list.
+/// </summary>
+public class StackOnList : IStack
+{
+    public StackOnList()
+    {
+        this.stack = new LinkedList<int>();
+    }
+
+    private LinkedList<int> stack;
+
+    /// <inheritdoc/>
+    public void Push(int value)
+    {
+        stack.AddFirst(value);
+    }
+
+    /// <inheritdoc/>
+    public bool IsEmpty()
+    {
+        return stack.First == null;
+    }
+
+    /// <inheritdoc/>
+    public int Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Cannot pop element from empty stack.");
+        }
+
+        int result = stack.First();
+        stack.RemoveFirst();
+
+        return result;
+    }
+}
