1a1bde4bd4cb2cc6c06f4b547b6da4e696baa810
DreDDeR <botayeverydaymyfriend@gmail.com>
2023-10-08 16:29:50 +0300

    transfering file to folders and creation the meat of the program, the ParseTree.cs

--- Homework 4/ParseTree/ParseTree/ParseTreeUtils/Operations/Subtraction.cs
+++ Homework 4/ParseTree/ParseTree/ParseTreeUtils/Operations/Subtraction.cs
@@ -1,21 +1,21 @@
 ﻿namespace ParseTree;
 
 /// <summary>
-/// Implementation of the "substraction" operation
+/// Implementation of the "subtraction" operation
 /// </summary>
-internal class Substraction : Operation
+internal class Subtraction : Operation
 {
     /// <summary>
-    /// The constructor of the Substraction, with the addition of two operands.
+    /// The constructor of the Subtraction, with the addition of two operands.
     /// </summary>
     /// <param name="leftChild"> Left operand. </param>
     /// <param name="rightChild"> Right operand. </param>
-    public Substraction(IParseTreeElement leftChild, IParseTreeElement rightChild) : base(leftChild, rightChild)
+    public Subtraction(IParseTreeElement leftChild, IParseTreeElement rightChild) : base(leftChild, rightChild)
     {
     }
 
     /// <summary>
-    /// Computing the substraction operation.
+    /// Computing the subtraction operation.
     /// </summary>
     /// <returns> Result of calculation. </returns>
     public override double Calculate() => LeftChild.Calculate() - RightChild.Calculate();
