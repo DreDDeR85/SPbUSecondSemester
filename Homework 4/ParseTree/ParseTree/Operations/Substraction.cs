f488238b2e36f3ce414e62c417a18fe6d082cce5
DreDDeR <botayeverydaymyfriend@gmail.com>
2023-10-08 15:39:17 +0300

    Adding operations functions for fourth hw

---
+++ Homework 4/ParseTree/ParseTree/Operations/Substraction.cs
@@ -0,0 +1,31 @@
+﻿namespace ParseTree;
+
+/// <summary>
+/// Implementation of the "substraction" operation
+/// </summary>
+internal class Substraction : Operation
+{
+    /// <summary>
+    /// The constructor of the Substraction, with the addition of two operands.
+    /// </summary>
+    /// <param name="leftChild"> Left operand. </param>
+    /// <param name="rightChild"> Right operand. </param>
+    public Substraction(IParseTreeElement leftChild, IParseTreeElement rightChild) : base(leftChild, rightChild)
+    {
+    }
+
+    /// <summary>
+    /// Computing the substraction operation.
+    /// </summary>
+    /// <returns> Result of calculation. </returns>
+    public override double Calculate() => LeftChild.Calculate() - RightChild.Calculate();
+
+    /// <summary>
+    /// Output like: "(- [left_value] [right_value])"
+    /// </summary>
+    public override void Print()
+    {
+        Console.Write("( - ");
+        base.Print();
+    }
+}
