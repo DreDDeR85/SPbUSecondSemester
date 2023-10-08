116e63e86f23335c7272f14694da73214f3a80d8
DreDDeR <botayeverydaymyfriend@gmail.com>
2023-10-09 02:11:58 +0300

    the main program added. tests remains

--- Homework 4/UniqueList/UniqueList/Program.cs
+++ Homework 4/UniqueList/UniqueList/Program.cs
@@ -1,2 +1,128 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+using Lists;
+
+static void PrintActions()
+{
+    WriteLine("""
+        0 - Exit
+        1 - Add an element
+        2 - Remove an element
+        3 - Change element by position
+        4 - Print the list
+        5 - View the list of commands
+        """);
+}
+
+WriteLine("I an a List data structure that does not store duplicate values. Below writed what can I do:");
+PrintActions();
+
+int action = 0;
+var isContinue = true;
+var uniqueList = new UniqueList();
+int value = 0;
+int index = 0;
+
+while (isContinue)
+{
+    Write("Enter the command number: ");
+    while (!int.TryParse(ReadLine(), out action) || action < 0 || action > 5)
+    {
+        WriteLine("Incorrect input! Enter only the number between 0 and 5.");
+        Write("Try again: ");
+    }
+
+    switch (action)
+    {
+        case 0:
+            WriteLine("Au revoir!");
+            isContinue = false;
+            break;
+
+        case 1:
+            Write("Enter a value: ");
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("incorrect input! Element can only be an integer.");
+                Console.Write("Try again: ");
+            }
+
+            try
+            {
+                uniqueList.Add(value);
+            }
+            catch (ElementAlreadyExistException)
+            {
+                WriteLine("Error! This element is already in the list.");
+                break;
+            }
+            WriteLine("The element was successfully added to the list.");
+            break;
+
+        case 2:
+            Write("Enter a value: ");
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Incorrect input! Element can only be an integer.");
+                Console.Write("Try again: ");
+            }
+
+            try
+            {
+                uniqueList.Remove(value);
+            }
+            catch (RemoveNonexistentElementException)
+            {
+                WriteLine("Error! There is no such item in the list.");
+                break;
+            }
+            WriteLine("The element was successfully removed from the list.");
+            break;
+
+        case 3:
+            Write("Enter the index of the element you want to replace: ");
+            while (!int.TryParse(Console.ReadLine(), out index))
+            {
+                Console.WriteLine("Incorrect input! Index can only be an integer.");
+                Console.Write("Try again: ");
+            }
+
+            Write($"Enter the value to which you wanna to change the element number {index}: ");
+            while (!int.TryParse(Console.ReadLine(),out value))
+            {
+                Console.WriteLine("Incorrect input! Element can only be an integer.");
+                Console.Write("Try again: ");
+            }    
+
+            try
+            {
+                if (uniqueList.Replace(value, index))
+                {
+                    WriteLine("The element was successfully replaced.");
+                }
+                else
+                {
+                    WriteLine("This index is not in the list");
+                }
+            }
+            catch (ElementAlreadyExistException)
+            {
+                WriteLine("Error! This element is already in the list.");
+                break;
+            }
+            break;
+
+        case 4:
+            Write("[ ");
+            foreach (var i in uniqueList.GetList())
+            {
+                Write($"{i} ");
+            }
+            WriteLine("]");
+            break;
+
+        case 5:
+            PrintActions();
+            break;
+    }
+}
